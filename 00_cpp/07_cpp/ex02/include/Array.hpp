ca9fe1bfbb5a6a4175ecf9232f656d47ab635bda
swl <julie.exe.franco@gmail.com>
2025-07-23 16:33:19 +0200

    EX02_START

---
+++ 00_cpp/07_cpp/ex02/include/Array.hpp
@@ -0,0 +1,26 @@
+#ifndef ARRAY_H
+#define ARRAY_H
+template <typename T>;
+class Array
+{
+   public:
+       /*♡♡♡♡♡♡♡♡♡♡♡CTOR♡♡♡♡♡♡♡♡♡♡♡♡♡*/
+       Array();  //cannon
+       Array(Array const & src);   //Cannon
+ 
+       /*♡♡♡♡♡♡♡♡♡♡♡GETTER♡♡♡♡♡♡♡♡♡♡♡♡♡*/
+ 
+       /*♡♡♡♡♡♡♡♡♡♡♡FT♡♡♡♡♡♡♡♡♡♡♡♡♡*/
+ 
+       /*♡♡♡♡♡♡♡♡♡♡♡OPERATOR♡♡♡♡♡♡♡♡♡♡♡♡♡*/
+       Array& operator=(Array const & rsh);    //Cannon
+ 
+       /*♡♡♡♡♡♡♡♡♡♡♡DTOR♡♡♡♡♡♡♡♡♡♡♡♡♡*/
+       virtual ~Array(); //Cannon
+  private:
+	   T	*_array;
+	   size_t	_size;
+};
+// std::ostream &operator<<(std::ostream &o, const Array &rhs);
+#endif // ARRAY_H
+
